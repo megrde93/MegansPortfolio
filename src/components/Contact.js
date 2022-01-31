d28a8eb15c0e1cc0b13b5cc41375fa690ccf8fba
Megan Deray <Megrde93@gmail.com>
2022-01-30 19:46:12 -0500

    contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Contact = () => {
   return (
     <div>
-      <form></form>
+      <h1>Contact me!</h1>
+      <Link>Email Me</Link>
+      <Link> Find me on LinkedIn</Link>
     </div>
   );
 };
