9e8d4a3b68532d5252846c8a3bd38dd29fe1ecdb
Megan Deray <Megrde93@gmail.com>
2022-02-03 21:11:28 -0500

    text edits

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -14,15 +14,15 @@ const AboutMe = () => {
           videos games, particularly strategy ones, and reading books of all
           types. I’ve been accused of being a horrible cook but an excellent
           baker, which confuses everyone, most of all me. I have a West Highland
-          Terrier named Buffy who adores me, and a cat named Mousse who mostly
-          tolerates my existence as a necessary part of dinner time.
+          who adores me, and a cat who mostly tolerates my existence as a
+          necessary part of dinner time.
         </h2>
         <h2>
           I started in web design after spending several years in Systems
           Technician jobs. I was motivated by my desire to create something new.
           My favorite technologies to work with are React, tailwind and square
           space. Are you looking for someone to work on a web development
-          project? Contact me to get started!{" "}
+          project? Contact me to get started!
         </h2>
         <img src={Megan} alt="Megan" />
         <Link to={Contact}>
