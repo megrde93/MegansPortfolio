dff8ab8c4128bb9ef96a499002a3537d38684ae4
Megan Deray <Megrde93@gmail.com>
2022-02-08 10:50:14 -0500

    home page updates

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Contact from "./Contact";
+import PastWork from "./PastWork";
 
 const HomePage = () => {
   return (
@@ -10,7 +11,20 @@ const HomePage = () => {
       <h2>
         I am a web developer who prioritizes beautiful, functional design.
       </h2>
+      <Link to={PastWork}>
+        <button> See My Past Work</button>
+      </Link>
       <div>
+        <h2>
+          I am currenlty working on React base projects with
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.linkedin.com/company/makeitmvp/"
+          >
+            MakeItMVP.
+          </a>
+        </h2>
         <h2>
           My specialties are front-end responsive design. I also work with work
           with squarespace. I am happy to work with you on designing your new
