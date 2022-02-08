be635944f876ae08b3787ba48fb19478d74a0841
Megan Deray <Megrde93@gmail.com>
2022-02-08 11:04:21 -0500

    homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -10,31 +10,36 @@ const HomePage = () => {
       <h1 className="text-2xl text-center	">
         Web Developer • IT Systems Admin • CompTIA Certified
       </h1>
-      <h2>
+      <h2 className="text-center	">
         I am a web developer who prioritizes beautiful, functional design.
       </h2>
-      <Link to={PastWork}>
-        <button> See My Past Work</button>
-      </Link>
-      <div>
-        <h2>
-          I am currenlty working on React base projects with
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.linkedin.com/company/makeitmvp/"
-          >
-            MakeItMVP.
-          </a>
-        </h2>
-        <h2>
-          My specialties are front-end responsive design. I also work with work
-          with squarespace. I am happy to work with you on designing your new
-          website. Contact me for more information.
-        </h2>
-      </div>
+
+      <h2>
+        I am currenlty working on React base projects with{" "}
+        <a
+          className="underline"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/company/makeitmvp/"
+        >
+          MakeItMVP.
+        </a>
+        <Link to={PastWork}>
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+            See My Past Work
+          </button>
+        </Link>
+      </h2>
+      <h2>
+        My specialties are front-end responsive design. I also work with work
+        with squarespace. I am happy to work with you on designing your new
+        website. Contact me for more information.
+      </h2>
+
       <Link to={Contact}>
-        <button> Contact Me</button>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+          Contact Me
+        </button>
       </Link>
     </div>
   );
